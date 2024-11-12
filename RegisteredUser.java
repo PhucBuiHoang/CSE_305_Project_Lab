a3f68df67eeb4b887e36c96a90a2911b80bcf366
Bùi Hoàng Phúc <phuc362001@gmail.com>
2024-11-12 09:00:21 +0700

    Add new class

---
+++ RegisteredUser.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+class RegisteredUser {
+    private List listOfUser;
+
+    public void addUser() {
+
+    }
+
+    public void removeUser() {
+
+    }
+}
---
+++ Tenant.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+class Tenant {
+    private User user;
+    private List listOfContract;
+
+    public void requestCreateRentalContract() {
+
+    }
+
+    public void requestTerminateRentalContract() {
+
+    }
+}
